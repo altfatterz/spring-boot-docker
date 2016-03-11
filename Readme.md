51d5b5c3268ca99a29a1fe29f08dec5c671beaa9
Zoltan Altfatter <altfatterz@gmail.com>
2016-03-11 12:18:55 +0100

    initial commit

---
+++ Readme.md
@@ -0,0 +1,48 @@
+Simple example dockerizing a Spring Boot service which leverages a mongodb storage.
+
+Start the containers with
+
+```
+docker-compose up
+```
+
+Add some data. Note, your docker host might have a different IP, check it with echo $DOCKER_HOST command.
+
+```
+echo '{"name":"St. Bonifatiuscollege ", "city":"Utrecht"}' | http post http://192.168.99.100:8080/schools
+```
+
+Query:
+
+```
+http get http://192.168.99.100:8080/schools/search/findByCityIgnoreCase\?city\=utrecht
+```
+
+```json
+{
+    "_embedded": {
+        "schools": [
+            {
+                "_links": {
+                    "school": {
+                        "href": "http://192.168.99.100:8080/schools/56e275b4e4b02d414c2d87bd"
+                    },
+                    "self": {
+                        "href": "http://192.168.99.100:8080/schools/56e275b4e4b02d414c2d87bd"
+                    }
+                },
+                "city": "Utrecht",
+                "name": "St. Bonifatiuscollege "
+            }
+        ]
+    },
+    "_links": {
+        "self": {
+            "href": "http://192.168.99.100:8080/schools/search/findByCityIgnoreCase?city=utrecht"
+        }
+    }
+}
+
+```
+
+In order to deploy it to AWS check the blog post:
---
+++ src/main/java/com/zoltanaltfatter/Application.java
@@ -0,0 +1,56 @@
+package com.zoltanaltfatter;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+@SpringBootApplication
+public class Application {
+
+	@Autowired
+	SchoolRepository repository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
+
+@Document(collection = "schools")
+class School {
+
+	@Id
+	private String id;
+
+	private final String name;
+	private final String city;
+
+	@JsonCreator
+	public School(@JsonProperty("name") String name, @JsonProperty("city") String city) {
+		this.name = name;
+		this.city = city;
+	}
+
+	public String getName() {
+		return name;
+	}
+	public String getCity() {
+		return city;
+	}
+}
+
+@RepositoryRestResource(collectionResourceRel = "schools", path = "/schools")
+interface SchoolRepository extends MongoRepository<School, String> {
+
+	List<School> findByCityIgnoreCase(@Param("city") String city);
+
+}
